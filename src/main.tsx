c4ab318d260a1ce48b434132c86f08cff0fbb3fa
Joshua Kelso <joshuas@kainos.com>
2025-03-28 13:05:52 +0000

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { Contact } from "./pages/Contact.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/me/",
+    element: <App />,
+    children: [
+      {
+        path: "/me/",
+        element: <Home />,
+      },
+      {
+        path: "/me/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
