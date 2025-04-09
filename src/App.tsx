2b45346917a67b2e7bb3e65c4eb99b3dde4295bc
Joshua Kelso <joshuas@kainos.com>
2025-04-09 17:10:03 +0100

    add download cv button

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Link, Outlet } from 'react-router-dom'
 import Grid from '@mui/material/Grid'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import { HeroHeader } from './components/HeroHeader'
 import { Badges } from './components/Badges'
 import { Socials } from './components/Socials'
+
+import DownloadIcon from '@mui/icons-material/Download';
 
 function App() {
   const year = new Date().getFullYear()
   const START_CAREER_YEAR = 2020
   const yearsExperience = year - START_CAREER_YEAR
+
+  const CV_URL = "https://1drv.ms/b/c/c486233ae5b6e226/ESbituU6I4YggMTwOgAAAAABhTY3dWcHLoA5S1k7pw0Uaw"
   return (
     <>
       <HeroHeader />
       <Box sx={{ margin: '0 auto', maxWidth: '1280px', textAlign: 'center' }} >
-        <Typography variant='h6' sx={{margin: {xs: 5, md:10}}} >
+        <Typography variant='h6' sx={{marginX: {xs: 5, md:10}}} marginTop={10} marginBottom={5} >
           Josh is a lead software engineer with {yearsExperience} year's technical industry experience with a 1st honour master's in computer science from <a href='https://www.qub.ac.uk/'>Queen's University Belfast</a>.  Having spent 2+ years in R'n'D Josh has a keen interest for innovation services, research, L&D and is equipped with initiative and drive to ensure that he capitalises on these new and emerging digital discoveries and curates them into new and existing client products.
         </Typography>
+
+        <Stack direction="row" spacing={2} justifyContent="center" sx={{ marginBottom: 5 }}>
+          <Button variant="outlined" href={CV_URL} target='_blank' endIcon={<DownloadIcon />}>Download CV</Button>
+        </Stack>
+
         <Box marginTop={5} sx={{padding: 2}}>
           <nav>
             <Link to="/me/">Highlights</Link>
