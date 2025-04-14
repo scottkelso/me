ceab6434a1785ac3a896cccaba480f2cd66bbc1b
Joshua Kelso <joshuas@kainos.com>
2025-04-14 11:17:52 +0100

    add green software to badges

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,21 +1,21 @@
-import { Grid } from "@mui/material";
+import { Stack } from "@mui/material";
 
 import awsImg from '../images/aws.png'
 import microsoftImg from '../images/microsoft.png'
 import ibmImg from '../images/ibm.png'
+import greensoftwareImg from '../images/green-software.png'
 
 export function Badges(){
   return (
-    <Grid container spacing={1} maxWidth={900} margin="auto" marginBottom={15}>
-      <Grid size={4}>
-        <img src={awsImg} alt="Logo" style={{ width: '100%', maxWidth: 150, height: 'auto'}} />
-      </Grid>
-      <Grid size={4}>
-        <img src={microsoftImg} alt="Logo" style={{ width: '100%', maxWidth: 150, height: 'auto'}}/>
-      </Grid>
-      <Grid size={4}>
-        <img src={ibmImg} alt="Logo" style={{ width: '100%', maxWidth: 150, height: 'auto'}} />
-      </Grid>
-    </Grid>
+    <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2} justifyContent="center" 
+      sx={{ 
+        marginBottom: 10,      
+        justifyContent: "center",
+        alignItems: "center" }}>
+        <img src={awsImg} alt="Logo" style={{ maxWidth: 150, height: 'auto'}} />
+        <img src={microsoftImg} alt="Logo" style={{ maxWidth: 150, height: 'auto'}}/>
+        <img src={ibmImg} alt="Logo" style={{ maxWidth: 150, height: 'auto'}} />
+        <img src={greensoftwareImg} alt="Logo" style={{ width: '100%', maxWidth: 150, height: 'auto'}} />
+    </Stack>
   )
 }
