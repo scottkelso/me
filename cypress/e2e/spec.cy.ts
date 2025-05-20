7fbf8a66414d47320b33c9a1b853bd7d68d4b52e
Joshua Kelso <joshuas@kainos.com>
2025-05-20 15:05:23 +0100

    add cypress tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,11 @@
+describe('Home page', () => {
+  it('Redirects to home page', () => {
+    cy.visit('http://localhost:5173/')
+    cy.url().should('include', 'localhost:5173/me')
+  })
+
+  it('Has name title', () => {
+    cy.visit('http://localhost:5173/')
+    cy.contains('Joshua Scott Kelso')
+  })
+})
